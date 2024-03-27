ae621931e7211e7fe5bd640648e2e05610710c56
Brandon Aldrich <brandon@jumperstudios.com>
2024-03-26 21:08:39 -0500

    chore: removing image

--- src/elements/InvoiceParts/Disclaimer.tsx
+++ src/elements/InvoiceParts/Disclaimer.tsx
@@ -28,7 +28,7 @@ const Disclaimer = () => (
     <View style={styles.container}>
         <Text style={styles.text}>A 1 1/2% FINANCE CHARGE (18% ANNUAL PERCENTAGE RATE) WILL BE ADDED TO ACCOUNTS WHICH ARE NOT PAID BY THE 30TH OF THE MONTH FOLLOWING THE DATE OF PURCHASE WITH A 50 CENTS MINIMUM CHARGE</Text>
         <Text style={styles.textLarge}>WE ACCEPT CREDIT CARDS</Text>
-        <Image src={'./public/cards.jpg'} style={{width: 300, height: 50}}/>
+        {/*<Image src={'/public/cards.jpg'} style={{width: 300, height: 50}}/>*/}
     </View>
 )
 
