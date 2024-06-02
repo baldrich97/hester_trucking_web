ed45d730ad19b4c471e0e7f0967528550f1af662
Brandon Aldrich <baldrich97@gmail.com>
2024-06-02 18:25:35 -0500

    chore: cron works locally at least

--- src/pages/api/crons/dataWipe.ts
+++ src/pages/api/crons/dataWipe.ts
@@ -1,24 +1,33 @@
 import {prisma} from "../../../server/db/client";
 
+import type { NextApiRequest, NextApiResponse } from 'next';
 
-
-export async function GET() {
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    await prisma.$executeRaw(`call wipe_data`);
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    await prisma.$executeRaw(`call wipe_customers`);
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    await prisma.$executeRaw(`call wipe_drivers`);
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    await prisma.$executeRaw(`call wipe_locations`);
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    await prisma.$executeRaw(`call trucks`);
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    await prisma.$executeRaw(`call trucks`);
+export default async function handler(
+    request: NextApiRequest,
+    response: NextApiResponse,
+) {
+    return new Promise<void>(async (resolve, reject) => {
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        await prisma.$executeRaw`call wipe_data`;
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        await prisma.$executeRaw`call wipe_customers`;
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        await prisma.$executeRaw`call wipe_drivers`;
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        await prisma.$executeRaw`call wipe_locations`;
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        await prisma.$executeRaw`call wipe_trucks`;
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        await prisma.$executeRaw`call wipe_types`;
+        response.statusCode = 200;
+        response.setHeader('Content-Type', 'application/json');
+        response.end(JSON.stringify({'success': true}))
+        resolve();
+    })
 }
