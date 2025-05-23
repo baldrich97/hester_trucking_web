e857487bdb41b7a02c8ee0f915f8e274ce3bc424
Brandon Aldrich <baldrich97@gmail.com>
2025-05-22 22:59:33 -0500

    fix: attempting to hard force

--- next-config.js
+++ next-config.js
@@ -1,4 +1,9 @@
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
     enabled: process.env.ANALYZE === 'true',
 })
-module.exports = withBundleAnalyzer({})
+module.exports = withBundleAnalyzer({
+    webpack: (config) => {
+        config.resolve.alias['@prisma/client'] = path.resolve(__dirname, 'prisma/generated/client');
+        return config;
+    },
+})
