3fbeb74301d5a478ea27f9af7a0dbfb902921549
Brandon Aldrich <baldrich97@gmail.com>
2025-05-23 00:14:12 -0500

    chore: removing all the nonsense

--- next-config.js
+++ next-config.js
@@ -1,9 +1,4 @@
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
     enabled: process.env.ANALYZE === 'true',
 })
-module.exports = withBundleAnalyzer({
-    webpack: (config) => {
-        config.resolve.alias['@prisma/client'] = path.resolve(__dirname, 'prisma/generated/client');
-        return config;
-    },
-})
+module.exports = withBundleAnalyzer({})
